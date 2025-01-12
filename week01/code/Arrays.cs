5b2ac33aaffb7e064646f5431acdfef8d31c4358
Matheus Felber <math.felber@gmail.com>
2025-01-12 14:30:10 -0300

    arrays passed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,10 +12,35 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
+        //PLAN: 
+        //1-Keep track of number and lenght (N, L), taking two integers.
+        //2-Create an empty array 
+        //3-Append N to the array. It will be on index 0
+        //4-Use the the number N, and lenght L on a loop. Loop "L" times if N is greater than 0. 
+        //3-Get the last integer on the array. Add LastDigit + N. (Do this on every loop, and append to the array). 
+        /// Example: N = 3, L=5
+        /// Create empty array
+        /// Append N to the array.
+        /// Create variable to keep track of the loop. keepTrack = 0
+        /// While keepTrack < L, Loop, and add 1 to Keeptrack on each loop:
+        /// 1ST LOOP: Array = [3] 
+        /// 2ND LOOP: Array = [3, 3+3]
+        /// 3RD LOOP: Array = [3, 3+3, 6+3] 
+        /// update-need to use multiplication instead to get the multiples.
+        //
 
-        return []; // replace this return statement with your own
+         double[] multiples = new double[length];
+         for (int i = 0; i < length; i++)
+        {
+            
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples; 
     }
 
+    
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
     /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
